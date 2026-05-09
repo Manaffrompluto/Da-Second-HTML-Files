636ad33bc69e5ad024b8d303c352c18f36613c4c
Manaffrompluto <manafabdullahramin@gmail.com>
2026-05-09 22:36:00 +0600

    naughty 2 project
    check naughty project first da one with no 2 also dis one is way less brutal with u

---
+++ file mikotis 2.py
@@ -0,0 +1,14 @@
+file_read = open('M:/Python/TEXTITOS MATITOS.txt','r')
+print("FILE IN READ MODITO")
+print(file_read.read())
+file_read.close()
+
+file_write = open('M:/Python/TEXTITOS MATITOS.txt', 'w')
+file_write.write(" idk what write mode is dis")
+file_write.write("                                                                                                                                                                                                                                                                                                                                     hello guy")
+file_write.close()
+
+file_append = open('M:/Python/TEXTITOS MATITOS.txt', 'a')
+file_append.write("\n WHO DARES TO READ DIS ABSOLUTELY NON TRUE THING WITHOUT HAVING A SINGLE EMOTION ARE DA GOAT")
+file_append.write("                                        ur father left u")
+file_append.close()
