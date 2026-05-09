2744a3ba3223c591fd386e9c336bebf7fdec61fa
Manaffrompluto <manafabdullahramin@gmail.com>
2026-05-09 22:22:14 +0600

    naughty project
    in dis project u have to first run da first file then open da second one for da surprise ( u dont wanna know what i wrote there and dont check code or dat will ruin surprise)

---
+++ file mikotis 1.py
@@ -0,0 +1,11 @@
+file = open('M:/Python/TEXTITOS MATITOS.txt', 'w')
+file.write("I WILL NERF UR ROBUX TO 2 IF U HAVE MORE THAN DAT")
+file.close()
+
+file = open('M:/Python/TEXTITOS MATITOS.txt', 'r')
+print(file.read())
+file.close()
+
+file = open('M:/Python/TEXTITOS MATITOS.txt', 'a')
+file.write("\n btw ur gay :D \n")
+file.close()
