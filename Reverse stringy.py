4b8df04467e0a6ad7a2ac02ece3526b5de4332a6
Manaffrompluto <manafabdullahramin@gmail.com>
2026-06-20 22:03:57 +0600

    Reverse string
    Thing dat reverses whatever da something u write

---
+++ Reverse stringy.py
@@ -0,0 +1,8 @@
+string = input("Pls enter ur own string: ")
+
+string2 = ('')
+for i in string:
+    string2 = i + string2
+
+print("\n Da orginal string = ", string)
+print("Da reversed string = ", string2)
