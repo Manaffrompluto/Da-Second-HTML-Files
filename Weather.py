0fe0bbe6e6ea3a75b8dbef19d8e1d0d7183bb4d2
Manaffrompluto <manafabdullahramin@gmail.com>
2026-07-10 22:03:55 +0600

    Weather
    stoopid project

---
+++ Weather.py
@@ -0,0 +1,7 @@
+def weather_condition():
+    print("The weather is pleasant in", autumn)
+    print("The weather is neutral in", spring)
+
+spring = "Autumn"
+autumn = "Spring"
+weather_condition()
