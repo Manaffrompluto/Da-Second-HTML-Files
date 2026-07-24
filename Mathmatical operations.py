46c9764ba7aeff59e2839bc39a338c4607d24bfb
Manaffrompluto <manafabdullahramin@gmail.com>
2026-07-24 22:16:08 +0600

    Mathematical operations
    stinky math stuff

---
+++ Mathmatical operations.py
@@ -0,0 +1,10 @@
+import math
+print('Da Floor and Ceiling value of 23.56 iz: ' + str(math.ceil(23.56)) + ', ' + str(math.floor(23.56)))
+
+x = 10
+y = -15
+print('Da value of X after copying da sign from Y iz: ' + str(math.copysign(x, y)))
+
+print('Da Absolute value of -96 and 56 iz: ' + str(math.fabs(-96)) + ', ' + str(math.fabs(56)))
+
+print('Da GCD of 24 and 56 iz: ' + str(math.gcd(24)) + ', ' + str(math.gcd(56)))
