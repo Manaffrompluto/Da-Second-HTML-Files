cf18dc5871574c2a6aeab13bcf7e9b9c6875d957
Manaffrompluto <manafabdullahramin@gmail.com>
2026-07-31 22:26:45 +0600

    Playing with lists
    Decided to do list stuff

--- Playing with lists.py
+++ Playing with lists.py
@@ -10,3 +10,19 @@ avg = count/len(l)
 
 print("Sum: ", count)
 print("Average: ", avg)
+
+l = [4, 5, 1, 2, 9, 7, 10, 8]
+print("List: ", l)
+
+count = 0
+
+for i in l:
+    count += i
+
+avg = count/len(l)
+
+print("Sum: ", count)
+print("Average: ", avg)
+
+print("Da smollest element iz: ", l[0])
+print("Da biggest element iz: ", l[-1])
