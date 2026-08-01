b96c147152b91d38bdeb8836da4de7e0f17108e6
Manaffrompluto <manafabdullahramin@gmail.com>
2026-08-01 21:52:46 +0600

    Tuplez
    Tuples in python

---
+++ Tuples.py
@@ -0,0 +1,18 @@
+tuple = ("tuple", False, 3.2, 1)
+print(tuple)
+
+tuple = (4, 6, 2, 8, 3, 1)
+print(tuple)
+
+tuple = tuple + (9,)
+print(tuple)
+
+tuple1 = (50, 10, 60, 70, )
+print(tuple1.count(50))
+
+tuple = (2, 4, 3, 5, 4, 6, 7, 8, 6, 1)
+_slice = tuple[3:5]
+print(_slice)
+
+_slice = tuple[:6]
+print(_slice)
