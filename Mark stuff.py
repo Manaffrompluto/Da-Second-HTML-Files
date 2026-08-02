53d5233963d3c45d30e59ecbbc6620397c526aee
Manaffrompluto <manafabdullahramin@gmail.com>
2026-08-02 16:47:49 +0600

    Mark stuff
    Avg marks and total marks nothing else

---
+++ Mark stuff.py
@@ -0,0 +1,11 @@
+list = (1, 10, 25, 50)
+print("Lists store data structures and variables in python.")
+
+count = 0
+for i in list:
+    count += 25
+
+avg = count/len(list)
+print("Da total mark iz ", count)
+print("Da average mark iz ", avg)
+
