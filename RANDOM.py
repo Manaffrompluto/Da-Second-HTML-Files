6923dd9e9ee4bc7eaf2b1377dd02ac84611a3758
Manaffrompluto <manafabdullahramin@gmail.com>
2026-08-07 18:59:29 +0600

    RANDOM
    random stuff going on here

---
+++ RANDOM.py
@@ -0,0 +1,26 @@
+import random
+print("iejnhwujisjwqsjwmswqjkmjkiJIJ")
+print("PAY TAX WHICH IZ 1000 DOLLA")
+inp = input("PAY")
+if inp == "1000":
+    print("ok")
+else:
+    print("BOMBACLANDA")
+
+
+try:
+    g = random.randint(1, 50)
+    print(g)
+    r = input("Guess num \n")
+    if int(r) == g:
+        print("correct now die")
+        print("UHDXNIUEWNDJOJOKMSDMCDSMKVMKJ")
+    else:
+        print("oh oh stinky u got it wrong so boom")
+
+except ValueError:
+    print("stinky value error") 
+except SyntaxError:
+    print(f"stinky syntax {r} {g} error")
+    print("gam gam star")
+    print("slice")
