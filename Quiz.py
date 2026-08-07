9570d8351bc34b79447fe92b7abb5b1d7885e4e7
Manaffrompluto <manafabdullahramin@gmail.com>
2026-08-07 22:50:21 +0600

    Quiz
    A quiz with five questions! To make it harder, every time u answer a question wrong u have to restart! NEW UPDATE: it now has da attempts feature!

--- Quiz.py
+++ Quiz.py
@@ -2,6 +2,7 @@ print("Quiz project!!!")
 print("There will be 5 questions, try to ans all of em!!!")
 print("Also, to make it harder, if u make a mistake, u restart all over again!")
 play = True
+attempt = 1
 
 while play:
     guess = input("What iz da best selling console of all time?: ")
@@ -14,19 +15,29 @@ while play:
             if guess2 == 'Peter parker' or guess2 == 'Peter Parker' or guess2 == 'peter parker':
                 print("Correct! Next question.")
                 guess3 = input("Wot was da first time period of Earth?: ")
-                if guess3 == 'Hadeon Eon' or guess3 == 'Hadeon eon' or guess3 == 'hadeon eon':
+                if guess3 == 'Hadean Eon' or guess3 == 'Hadean eon' or guess3 == 'hadean eon':
                     print("Correct! Last question.")
                     guess4 = input("Wot was da most terrifying dinosaur?: ")
                     if guess4 == 'Tyrannosaurus' or guess4 == 'tyrannosaurus':
                         print("Correct! U win!!!")
+                        if attempt == 1:
+                            print(f"It took ya {attempt} attempt!")
+                        else:
+                            print(f"It took ya {attempt} attempts!")
                         break
+
                     else:
                         print("Incorrect! Restart!")
+                        attempt += 1
                 else:
                     print("Incorrect! Restart!")
+                    attempt += 1
             else:
                 print("Incorrect! Restart!")
+                attempt += 1
         else:
             print("Incorrect! Restart!")
+            attempt += 1
     else:
-        print("Incorrect! Try again!")          
+        print("Incorrect! Try again!")  
+        attempt += 1            
